2c7c2c444029c7950198fda3ad4d72e45fdbad61
tsukuda1998 <tsukuda1998@gmail.com>
2024-12-01 19:20:23 +0900

    Initial commit

---
+++ wp-content/themes/mytheme/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_title('<h2>', '</h2>');
+            the_content();
+        endwhile;
+    else :
+        echo '<p>No content found</p>';
+    endif;
+    ?>
+    <?php wp_footer(); ?>
+</body>
+</html>
+
